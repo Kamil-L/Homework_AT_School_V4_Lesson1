4951cedaf30c2e97775e4703655f81ece949a69c
klont <klont@sii.pl>
2022-08-10 09:59:43 +0200

    HomeworkLesson1 - after Mateusz correction

--- src/HomeworkLesson1.java
+++ src/HomeworkLesson1.java
@@ -8,6 +8,7 @@ public class HomeworkLesson1 {
         int numToGuess = new Random().nextInt(100);
         Scanner scannerNumber = new Scanner(System.in);
         System.out.println("Please input integer number from the following range 0-99: ");
+        System.out.println(numToGuess);
 
         int numChosen, numUsedTries, numOfTries = 5;
 
@@ -16,23 +17,21 @@ public class HomeworkLesson1 {
             numChosen = scannerNumber.nextInt();
             numUsedTries = numOfTries - i;
 
-            if (numChosen != numToGuess) {
-
-                if (numUsedTries == 0) {
-                    System.out.println("Sorry you didn't guess the number, the answer was: " + numToGuess);
-                    break;
-                } else if (numChosen > numToGuess) {
-                    System.out.println("Your number is GREATER than the one you are trying to guess");
-                } else
-                    System.out.println("Your number is LOWER than the one you are trying to guess");
-
-                System.out.println("You have " + numUsedTries + " attempts left!");
-                System.out.println("Please try again");
-
-            } else {
+            if (numChosen == numToGuess) {
                 System.out.println("You guessed it!");
                 break;
-            }
+            } else if (numUsedTries == 0) {
+                System.out.println("Sorry you didn't guess the number, the answer was: " + numToGuess);
+                break;
+            } else if (numChosen > numToGuess) {
+                System.out.println("Your number is GREATER than the one you are trying to guess");
+            } else
+                System.out.println("Your number is LOWER than the one you are trying to guess");
+
+            System.out.println("You have " + numUsedTries + " attempts left!");
+            System.out.println("Please try again");
+
+
         }
     }
 }
